3daa95d741124c0958501fecff8ff6e3b0b9ad6d
FrankIglesias <ifrancisco.iglesias@gmail.com>
2021-03-28 11:16:30 -0300

    Initial commit

---
+++ api/place.js
@@ -0,0 +1,11 @@
+const axios = require('axios');
+
+module.exports = async (req, res) => {
+  const { place_id } = req.query;
+  console.log(`[${new Date()}] - Search for ${place_id}`);
+  const response = await axios.get('https://maps.googleapis.com/maps/api/geocode/json', { params: {
+    place_id,
+    key: process.env.NODE_MAPS_SEARCH_API
+  }})
+  res.send(response.data);
+}
