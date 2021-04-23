6688451118891d42bc62868121f6c2fbaae6fddc
FrankIglesias <ifrancisco.iglesias@gmail.com>
2021-04-23 15:15:39 -0300

    Corrected photo proxy

--- api/photo.js
+++ api/photo.js
@@ -5,4 +5,8 @@ module.exports = createProxyMiddleware({
   target: 'https://maps.googleapis.com',
   changeOrigin: true,
   pathRewrite: (_, req) => `/maps/api/place/photo?photoreference=${req.query.photo_reference}&maxwidth=400&key=${process.env.NODE_MAPS_SEARCH_API}`,
+  onProxyRes: proxyRes  => {
+    proxyRes.headers['cache-control'] = 's-maxage=2678000';
+    proxyRes.statusCode = 301;
+  }
 });
